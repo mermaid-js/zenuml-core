b9983e92bee39ccc966fbfef57d4760f1aeebead
MrCoder <eagle.xiao@gmail.com>
2023-06-07 21:07:36 +1000

    Added FrameBorder to calculate the border width of frames. (Frames can be used for fragments and the diagram frame.)

---
+++ src/positioning/FrameBorder.spec.ts
@@ -0,0 +1,69 @@
+import FrameBorder from './FrameBorder';
+describe('FrameBorder', function () {
+  it('1 point 1 group', function () {
+    const frame = { left: 'a', right: 'a' };
+    expect(FrameBorder(frame)).toEqual({ left: 10, right: 10 });
+  });
+  it('1 point 2 groups', function () {
+    const frame = {
+      left: 'a',
+      right: 'a',
+      children: [
+        { left: 'a', right: 'a' }
+      ]
+    };
+    expect(FrameBorder(frame)).toEqual({ left: 20, right: 20 });
+  });
+
+  it('2 points 2 groups', function () {
+    const frame = {
+      left: 'a',
+      right: 'b',
+      children: [
+        { left: 'a', right: 'b' }
+      ]
+    };
+    expect(FrameBorder(frame)).toEqual({ left: 20, right: 20 });
+  });
+
+  it('2 points 2 groups', function () {
+    const frame = {
+      left: 'a',
+      right: 'b',
+      children: [
+        { left: 'b', right: 'b' }
+      ]
+    };
+    expect(FrameBorder(frame)).toEqual({ left: 10, right: 20 });
+  });
+
+  it('2 points 3 groups', function () {
+    const frame = {
+      left: 'a',
+      right: 'b',
+      children: [
+        { left: 'a', right: 'a' },
+        { left: 'b', right: 'b' }
+      ]
+    };
+    expect(FrameBorder(frame)).toEqual({ left: 20, right: 20 });
+  });
+
+  it('a lot points a lot groups', function () {
+    const frame = {
+      left: 'a',
+      right: 'd',
+      children: [
+        { left: 'b', right: 'b' },
+        { left: 'b', right: 'd', children: [{ left: 'b', right: 'c' }, { left: 'c', right: 'd' }] },
+        { left: 'b', right: 'c' },
+        { left: 'a', right: 'b' },
+        { left: 'c', right: 'c' },
+        { left: 'd', right: 'd', children: [{ left: 'd', right: 'd', children: [{ left: 'd', right: 'd' }] }] },
+        { left: 'd', right: 'd' },
+        { left: 'd', right: 'd' },
+      ]
+    };
+    expect(FrameBorder(frame)).toEqual({ left: 20, right: 40 });
+  });
+});
---
+++ src/positioning/FrameBorder.ts
@@ -0,0 +1,34 @@
+interface Frame {
+  left: string;
+  right: string;
+  children?: Frame[];
+}
+
+enum PathType {
+  LEFT = 'LEFT',
+  RIGHT = 'RIGHT',
+}
+
+function longestPath(frame: Frame, pathType: PathType): number {
+  if (!frame.children || frame.children.length === 0) {
+    return 1;
+  }
+
+  let maxDepth = 0;
+  for (let child of frame.children) {
+    if ((pathType === PathType.LEFT && child.left !== frame.left) ||
+      (pathType === PathType.RIGHT && child.right !== frame.right)) {
+      continue;
+    }
+    maxDepth = Math.max(maxDepth, longestPath(child, pathType));
+  }
+
+  return maxDepth + 1;
+}
+
+export default function FrameBorder(frame: Frame) {
+  return {
+    left: 10 * longestPath(frame, PathType.LEFT),
+    right: 10 * longestPath(frame, PathType.RIGHT),
+  };
+}
