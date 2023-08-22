39f66bb1aa6e0c88e404daaeab5e57d017a80998
unimu-cc <kkcarson1874@gmail.com>
2023-08-22 14:49:36 +0800

    fix: theme select style

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,6 +66,7 @@ module.exports = {
         },
       },
       borderColor: {
+        primary: 'var(--color-border-primary)',
         skin: {
           frame: 'var(--color-border-frame, var(--color-border-base, #000))',
           participant:
