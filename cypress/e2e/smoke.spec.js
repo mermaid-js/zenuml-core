d1387c017e2b51df30c79a56475c8efadeb4554c
MrCoder <eagle.xiao@gmail.com>
2023-09-20 21:27:22 +1000

    Wait for the privacy icon to be shown before screenshotting.

--- cypress/e2e/smoke.spec.js
+++ cypress/e2e/smoke.spec.js
@@ -4,6 +4,7 @@ describe('Smoke test', function () {
   it('should load the home page', function () {
     cy.visit('http://127.0.0.1:8080/cy/smoke.html');
     cy.get('[data-signature="append(line)"]', { timeout: 5000 }).should('be.visible');
+    cy.get('.privacy>span>svg', { timeout: 5000 }).should('be.visible');
     cy.document().toMatchImageSnapshot({
       imageConfig: { threshold: 0.012 },
       capture: 'viewport',
