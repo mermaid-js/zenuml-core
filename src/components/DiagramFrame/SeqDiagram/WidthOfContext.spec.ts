82accf6015a6300f14923a0ef79ee7ff50a909c7
unimu-cc <kkcarson1874@gmail.com>
2023-11-23 17:50:58 +0800

    test: update test cases

--- src/components/DiagramFrame/SeqDiagram/WidthOfContext.spec.ts
+++ src/components/DiagramFrame/SeqDiagram/WidthOfContext.spec.ts
@@ -1,58 +1,57 @@
-import { TotalWidth} from "./WidthOfContext";
-import {Fixture} from "../../../../test/unit/parser/fixture/Fixture";
-import { Coordinates } from '@/positioning/Coordinates';
-import { stubWidthProvider } from '@/../test/unit/parser/fixture/Fixture';
-import {RootContext} from "@/parser";
+import { TotalWidth } from "./WidthOfContext";
+import { Fixture } from "../../../../test/unit/parser/fixture/Fixture";
+import { Coordinates } from "@/positioning/Coordinates";
+import { stubWidthProvider } from "@/../test/unit/parser/fixture/Fixture";
+import { RootContext } from "@/parser";
 
 vi.mock("@/positioning/WidthProviderFunc", () => {
   return {
     default: (text: string) => {
-      const number = parseInt(text.trim().substring(1) || '0');
+      const number = parseInt(text.trim().substring(1) || "0");
       return isNaN(number) ? 0 : number;
-    }
-  }
+    },
+  };
 });
 
-describe('TotalWidth', function () {
-
-  test('TotalWidth', () => {
-    const code = 'A.method';
-    let rootContext = RootContext(code);
+describe("TotalWidth", function () {
+  test("TotalWidth", () => {
+    const code = "A.method";
+    const rootContext = RootContext(code);
     const coordinates = new Coordinates(rootContext, stubWidthProvider);
     const ctx = Fixture.firstStatement(code);
     // _STARTER_ is taken into account
-    expect(TotalWidth(ctx, coordinates)).toBe(170);
-  })
+    expect(TotalWidth(ctx, coordinates)).toBe(130);
+  });
 
-  test('TotalWidth with depth', () => {
-    const code = 'if(x) {A.method}';
-    let rootContext = RootContext(code);
+  test("TotalWidth with depth", () => {
+    const code = "if(x) {A.method}";
+    const rootContext = RootContext(code);
     const coordinates = new Coordinates(rootContext, stubWidthProvider);
     const ctx = Fixture.firstStatement(code);
-    expect(TotalWidth(ctx, coordinates)).toBe(190);
-  })
+    expect(TotalWidth(ctx, coordinates)).toBe(150);
+  });
 
-  test('TotalWidth with distance', () => {
-    const code = 'if(x) {A.method; B.method}';
-    let rootContext = RootContext(code);
+  test("TotalWidth with distance", () => {
+    const code = "if(x) {A.method; B.method}";
+    const rootContext = RootContext(code);
     const coordinates = new Coordinates(rootContext, stubWidthProvider);
     const ctx = Fixture.firstStatement(code);
-    expect(TotalWidth(ctx, coordinates)).toBe(350);
-  })
+    expect(TotalWidth(ctx, coordinates)).toBe(270);
+  });
 
-  test('TotalWidth with extra width from self message', () => {
-    const code = 'if(x) {A.method; B.method { s100 }}';
-    let rootContext = RootContext(code);
+  test("TotalWidth with extra width from self message", () => {
+    const code = "if(x) {A.method; B.method { s100 }}";
+    const rootContext = RootContext(code);
     const coordinates = new Coordinates(rootContext, stubWidthProvider);
     const ctx = Fixture.firstStatement(code);
-    expect(TotalWidth(ctx, coordinates)).toBe(407);
-  })
+    expect(TotalWidth(ctx, coordinates)).toBe(347);
+  });
 
-  test('TotalWidth with extra width from self message2', () => {
-    const code = 'if(x) {A1.method; B1.method { s200 } C1.method { s10 }}';
-    let rootContext = RootContext(code);
+  test("TotalWidth with extra width from self message2", () => {
+    const code = "if(x) {A1.method; B1.method { s200 } C1.method { s10 }}";
+    const rootContext = RootContext(code);
     const coordinates = new Coordinates(rootContext, stubWidthProvider);
     const ctx = Fixture.firstStatement(code);
-    expect(TotalWidth(ctx, coordinates)).toBe(510);
-  })
+    expect(TotalWidth(ctx, coordinates)).toBe(447);
+  });
 });
