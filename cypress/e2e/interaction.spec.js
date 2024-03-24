ee378c5b90ed9907816bb57a502dcc5a77bdbea0
dontry <mccoy018@gmail.com>
2024-03-24 15:13:22 +1100

    Add edit message test case to cypress tests

--- cypress/e2e/interaction.spec.js
+++ cypress/e2e/interaction.spec.js
@@ -1,13 +1,21 @@
 /* eslint-disable no-undef */
-import 'cypress-plugin-snapshots/commands';
-describe('Smoke test', function () {
-  it('interaction', function () {
-    cy.visit('http://127.0.0.1:8080/cy/smoke-interaction.html');
+import "cypress-plugin-snapshots/commands";
+describe("Smoke test", function () {
+  it("interaction", function () {
+    cy.visit("http://127.0.0.1:8080/cy/smoke-interaction.html");
     // This line is to make sure the privacy icon is loaded
-    cy.get('.privacy>span>svg', { timeout: 5000 }).should('be.visible');
+    cy.get(".privacy>span>svg", { timeout: 5000 }).should("be.visible");
+
+    // Edit the message
+    const messageLabel = cy.contains("ASelf");
+    messageLabel.dblclick();
+    messageLabel.type("A{enter}");
+    cy.contains("ASelfA").should("be.visible");
+    cy.get("body").click();
+
     cy.document().toMatchImageSnapshot({
       imageConfig: { threshold: 0.01 },
-      capture: 'viewport',
+      capture: "viewport",
     });
   });
 });
