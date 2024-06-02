73bb6a112e86ac434b609be2ea1bd799d391309f
MrCoder <eagle.xiao@gmail.com>
2024-06-02 15:58:05 +1000

    Added e2e test for async messages.

---
+++ cypress/e2e/async-message.spec.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+import "cypress-plugin-snapshots/commands";
+describe("Rendering", function () {
+  it("Async message - 1", function () {
+    cy.visit("http://127.0.0.1:8080/cy/async-message-1.html");
+    // This line is to make sure the privacy icon is loaded
+    cy.get(".privacy>span>svg", { timeout: 5000 }).should("be.visible");
+    cy.document().toMatchImageSnapshot({
+      imageConfig: { threshold: 0.01 },
+      capture: "viewport",
+    });
+  });
+  it("Async message - 2", function () {
+    cy.visit("http://127.0.0.1:8080/cy/async-message-2.html");
+    // This line is to make sure the privacy icon is loaded
+    cy.get(".privacy>span>svg", { timeout: 5000 }).should("be.visible");
+    cy.document().toMatchImageSnapshot({
+      imageConfig: { threshold: 0.01 },
+      capture: "viewport",
+    });
+  });
+  it("Async message - 3", function () {
+    cy.visit("http://127.0.0.1:8080/cy/async-message-3.html");
+    // This line is to make sure the privacy icon is loaded
+    cy.get(".privacy>span>svg", { timeout: 5000 }).should("be.visible");
+    cy.document().toMatchImageSnapshot({
+      imageConfig: { threshold: 0.01 },
+      capture: "viewport",
+    });
+  });
+});
