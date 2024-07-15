699ec2c5af8ccd9a1f69606cfa4fbbf29ff53f7d
dontry <mccoy018@gmail.com>
2024-07-15 22:51:45 +1000

    Refactor EventBus to fix Vue Warning

--- src/EventBus.ts
+++ src/EventBus.ts
@@ -1,8 +1,35 @@
-import Vue from "vue";
-export default new Vue();
+type Callback = (...args: any[]) => any;
+class EventEmitter {
+  private events: { [key: string]: Set<Callback> } = {};
+
+  on(event: string, callback: Callback) {
+    if (!this.events[event]) {
+      this.events[event] = new Set<Callback>();
+    }
+    console.log(`Event ${event} ${callback} added`);
+    console.log(this.events);
+    this.events[event].add(callback);
+  }
+
+  off(event: string, callback: Callback) {
+    console.log(`Event ${event} ${callback} removed`);
+    console.log(this.events);
+    this.events[event]?.delete(callback);
+  }
+
+  emit(event: string, data?: any) {
+    const callbacks = this.events[event];
+    if (callbacks) {
+      callbacks.forEach((callback) => callback(data));
+    }
+  }
+}
+
+export const EventBus = new EventEmitter();
 
 export function CustomEmit(store: any, event: string, data: any) {
-  store.commit("eventEmit", { event: event, data: data });
+  store.commit("eventEmit", { event, data });
+  EventBus.emit(event, data);
 }
 
 export function TrackEvent(
@@ -11,8 +38,7 @@ export function TrackEvent(
   action: string,
   category: string,
 ) {
-  store.commit("eventEmit", {
-    event: "trackEvent",
-    data: { label: JSON.stringify(label), action, category },
-  });
+  const trackData = { label: JSON.stringify(label), action, category };
+  store.commit("eventEmit", { event: "trackEvent", data: trackData });
+  EventBus.emit("trackEvent", trackData);
 }
