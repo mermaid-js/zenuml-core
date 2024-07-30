6e1a28fb42565c6b3a4df6924d9f949eabfc9be6
dontry <mccoy018@gmail.com>
2024-07-30 20:54:43 +1000

    extended the timeout of async-message e2e test

--- cypress/e2e/async-message.spec.js
+++ cypress/e2e/async-message.spec.js
@@ -4,7 +4,7 @@ describe("Rendering", function () {
   it("Async message - 1", function () {
     cy.visit("http://127.0.0.1:8080/cy/async-message-1.html");
     // This line is to make sure the privacy icon is loaded
-    cy.get(".privacy>span>svg", { timeout: 5000 }).should("be.visible");
+    cy.get(".privacy>span>svg", { timeout: 9000 }).should("be.visible");
     cy.document().toMatchImageSnapshot({
       imageConfig: { threshold: 0.01 },
       capture: "viewport",
