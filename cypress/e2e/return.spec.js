201cfcc6aef9641de30391b696529a23c1567361
MrCoder <eagle.xiao@gmail.com>
2024-10-31 19:11:48 +1100

    Updated cy test for 'return'.

--- cypress/e2e/return.spec.js
+++ cypress/e2e/return.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
 import "cypress-plugin-snapshots/commands";
-describe("Smoke test", function () {
+describe("Return functionality", function () {
   it("return", function () {
-    cy.visit("http://127.0.0.1:8080/cy/smoke-return.html");
+    cy.visit("http://127.0.0.1:8080/cy/return.html");
     // This line is to make sure the privacy icon is loaded
     cy.get(".privacy>span>svg", { timeout: 5000 }).should("be.visible");
     cy.document().toMatchImageSnapshot({
