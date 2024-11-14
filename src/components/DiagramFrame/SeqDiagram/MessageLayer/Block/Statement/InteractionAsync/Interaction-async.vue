e9c36c7fdf794681772210628dfad180a033a62c
MrCoder <eagle.xiao@gmail.com>
2024-11-14 20:27:23 +1100

    Fixed an issue of out-of-band async messages.

--- src/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/InteractionAsync/Interaction-async.vue
+++ src/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/InteractionAsync/Interaction-async.vue
@@ -4,6 +4,8 @@
 -->
 <template>
   <div
+    :data-origin="origin"
+    :data-out-of-band="outOfBand"
     class="interaction async"
     v-on:click.stop="onClick"
     :data-signature="signature"
@@ -47,6 +49,7 @@ import Message from "../Message/Message.vue";
 import { mapGetters } from "vuex";
 import { CodeRange } from "@/parser/CodeRange";
 import ArrowMixin from "@/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/ArrowMixin";
+import { LIFELINE_WIDTH } from "@/positioning/Constants";
 
 function isNullOrUndefined(value) {
   return value === null || value === undefined;
@@ -64,6 +67,9 @@ export default {
     asyncMessage: function () {
       return this.context?.asyncMessage();
     },
+    outOfBand: function () {
+      return this.source !== this.origin;
+    },
     interactionWidth: function () {
       if (this.isSelf) {
         const leftOfMessage = 100;
@@ -72,7 +78,12 @@ export default {
           averageWidthOfChar * (this.signature?.length || 0) + leftOfMessage
         );
       }
-      return Math.abs(this.distance(this.target, this.source));
+      let safeOffset = this.outOfBand ? 0 : this.selfCallIndent || 0;
+
+      return (
+        Math.abs(this.distance(this.target, this.source) - safeOffset) -
+        LIFELINE_WIDTH
+      );
     },
     // Both 'left' and 'translateX' can be used to move the element horizontally.
     // Change it to use translate according to https://stackoverflow.com/a/53892597/529187.
@@ -80,9 +91,10 @@ export default {
       if (!this.from) {
         return 0;
       }
+      let safeOffset = this.outOfBand ? this.selfCallIndent || 0 : 0;
       return this.rightToLeft
         ? this.distance(this.target, this.from)
-        : this.distance(this.source, this.from);
+        : this.distance(this.source, this.from) - safeOffset;
     },
     rightToLeft: function () {
       return this.distance(this.target, this.source) < 0;
