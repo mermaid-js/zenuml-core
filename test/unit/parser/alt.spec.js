5573c2fad3f297d2f0b7ac3944e4f519177d025f
MrCoder <eagle.xiao@gmail.com>
2024-11-16 09:52:26 +1100

    Self Message Assignment at root alt.

--- test/unit/parser/alt.spec.js
+++ test/unit/parser/alt.spec.js
@@ -1,34 +1,43 @@
-import { Fixture } from './fixture/Fixture';
+import { Fixture } from "./fixture/Fixture";
 
-test('Empty `if`', () => {
-  let alt = Fixture.firstStatement('if(x) {}').alt();
-  expectText(alt).toBe('if(x){}');
+test("Empty `if`", () => {
+  let alt = Fixture.firstStatement("if(x) {}").alt();
+  expectText(alt).toBe("if(x){}");
   let ifBlock = alt.ifBlock();
-  expectText(ifBlock).toBe('if(x){}');
-  expectText(ifBlock.parExpr()).toBe('(x)');
-  expectText(ifBlock.braceBlock()).toBe('{}');
+  expectText(ifBlock).toBe("if(x){}");
+  expectText(ifBlock.parExpr()).toBe("(x)");
+  expectText(ifBlock.braceBlock()).toBe("{}");
 });
 
-test('`if` with a block', () => {
-  let braceBlock = braceBlockOfIf('if(x) { doSomething() \n\r}');
-  expectText(braceBlock.block().stat()[0]).toBe('doSomething()');
+test("`if` with a block", () => {
+  let braceBlock = braceBlockOfIf(`if(x) {
+    result = doSomething()  // <- statElement
+  }`);
+  const statElement = braceBlock.block().stat()[0];
+  expectText(statElement).toBe("result=doSomething()");
+  expect(statElement.message().SignatureText()).toBe("doSomething()");
 });
 
-test('`if` with comments only', () => {
-  let braceBlock = braceBlockOfIf('if(x) { // comment \n\r}');
-  expect(braceBlock.getComment()).toBe(' comment \n');
+test("`if` with comments only", () => {
+  let braceBlock = braceBlockOfIf("if(x) { // comment \n\r}");
+  expect(braceBlock.getComment()).toBe(" comment \n");
 });
 
-test('`if` with comments and a block', () => {
-  let braceBlock = braceBlockOfIf('if(x) { // comment \n\r doSomething \n\r}');
+test("`if` with comments and a block", () => {
+  let braceBlock = braceBlockOfIf("if(x) { // comment \n\r doSomething \n\r}");
   expect(braceBlock.getComment()).toBeNull();
 });
 
-describe('if - incomplete', () => {
-  test('', () => {
-    expect(Fixture.firstStatement('if(x)').alt().ifBlock().parExpr().condition().getText()).toBe(
-      'x'
-    );
+describe("if - incomplete", () => {
+  test("", () => {
+    expect(
+      Fixture.firstStatement("if(x)")
+        .alt()
+        .ifBlock()
+        .parExpr()
+        .condition()
+        .getText(),
+    ).toBe("x");
   });
 });
 
