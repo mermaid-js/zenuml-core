6f0d52039c4b92974ad0a3c1c8e9f8aaeac70e4d
MrCoder <eagle.xiao@gmail.com>
2024-11-21 11:37:12 +1100

    Add null check for predicate in getAncestors method and improve tests.

--- src/parser/AncestorPath.spec.ts
+++ src/parser/AncestorPath.spec.ts
@@ -4,16 +4,24 @@ import sequenceParser from "@/generated-parser/sequenceParser";
 import antlr4 from "antlr4";
 
 describe("Ancestor", () => {
-  it("should return the ancestor path", () => {
-    const firstChild = Fixture.firstChild("A.method() { C->C.method }");
-    expect(firstChild.getFormattedText()).toBe("C->C.method");
+  // preparation
+  const firstChild = Fixture.firstChild("A.method() { C->C.method }");
+  expect(firstChild.getFormattedText()).toBe("C->C.method");
 
+  it("Null predict", () => {
+    const ancestors = firstChild.getAncestors();
+    expect(ancestors.length).toBe(7);
+    expect(ancestors[0].constructor.name).toBe("StatContext");
+  });
+
+  it("should return the ancestor path", () => {
     function predict(ctx: antlr4.ParserRuleContext) {
       return ctx instanceof sequenceParser.MessageContext;
     }
 
     const ancestors = firstChild.getAncestors(predict);
     expect(ancestors.length).toBe(1);
+    expect(ancestors[0].constructor.name).toBe("MessageContext");
     expect(ancestors[0].getFormattedText()).toBe("A.method() { C->C.method }");
   });
 });
--- src/parser/AncestorPath.ts
+++ src/parser/AncestorPath.ts
@@ -2,12 +2,12 @@ import { default as antlr4 } from "antlr4";
 
 // TypeScript cannot infer that the method exists on the prototype chain
 (antlr4.ParserRuleContext.prototype as any).getAncestors = function (
-  predicate: (ctx: antlr4.ParserRuleContext) => boolean,
+  predicate?: (ctx: antlr4.ParserRuleContext) => boolean,
 ): antlr4.ParserRuleContext[] {
   let current = this;
   const ancestors = [];
   while (current) {
-    if (predicate(current)) {
+    if (!predicate || predicate(current)) {
       ancestors.push(current);
     }
     current = current.parentCtx;
