443bb38993d9bed9c2392751a2ae0575b0121081
MrCoder <eagle.xiao@gmail.com>
2024-11-21 11:29:36 +1100

    Add type casting to antlr4.ParserRuleContext.prototype method

--- src/parser/AncestorPath.ts
+++ src/parser/AncestorPath.ts
@@ -1,16 +1,15 @@
 import { default as antlr4 } from "antlr4";
-// @ts-ignore
-antlr4.ParserRuleContext.prototype.getAncestors = function (
+
+// TypeScript cannot infer that the method exists on the prototype chain
+(antlr4.ParserRuleContext.prototype as any).getAncestors = function (
   predicate: (ctx: antlr4.ParserRuleContext) => boolean,
 ): antlr4.ParserRuleContext[] {
-  // @ts-nocheck
   let current = this;
   const ancestors = [];
   while (current) {
     if (predicate(current)) {
       ancestors.push(current);
     }
-    // @ts-ignore
     current = current.parentCtx;
   }
   return ancestors;
