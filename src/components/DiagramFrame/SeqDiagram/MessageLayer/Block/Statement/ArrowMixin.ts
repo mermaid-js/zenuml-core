a2601531be591494db1957873e340f4f0728d048
MrCoder <eagle.xiao@gmail.com>
2024-11-22 14:19:38 +1100

    Removed xxxOffset.

--- src/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/ArrowMixin.ts
+++ src/components/DiagramFrame/SeqDiagram/MessageLayer/Block/Statement/ArrowMixin.ts
@@ -1,6 +1,5 @@
 import { defineComponent } from "vue";
 import sequenceParser from "@/generated-parser/sequenceParser";
-import { OCCURRENCE_BAR_SIDE_WIDTH } from "@/positioning/Constants";
 import { mapGetters } from "vuex";
 import Anchor2 from "@/positioning/Anchor2";
 // Define the context type
@@ -62,53 +61,6 @@ export default defineComponent({
 
     interactionWidth: function (): number {
       return Math.abs(this.anchor2Source.edgeOffset(this.anchor2Target));
-    },
-    /**
-     * The offset is to make sure the sub-occurrence bar is not fully layered
-     * on top of the main occurrence bar. To achieve this, whenever a participant
-     * is re-entered, the offset increases by 7px.
-     *
-     * There are two ways to re-enter a participant:
-     *
-     * 1. A.m { s }
-     * 2. B A A.m { B->A.m }
-     *
-     * #1 is the most common case.
-     *
-     * If each Interaction keeps a stack of participants, we would be able to know
-     * the depth of the stack on one given participant. This would allow us to
-     * calculate the offset.
-     * For example,
-     *
-     *                          stack       offset S     offset A         offset B
-     * A.m                      [A]         0            0                NA
-     * A->B.m                   [B]         NA           NA               7
-     * A.m { s }                [A A]       0            7                NA
-     * A.m { s { s } }          [A A A]     0            14               NA
-     * A.m { s { B.m } }        [A A B]     0            14               7
-     * A.m { B->A.m }           [A A]       0            14               NA
-     * A.m { B.m { A.m } }      [A B A]     0            14               7
-     * B A A.m { B->A.m }       [A A]       0            14               NA
-     *
-     * If offset is NA, it is effectively 0.
-     *
-     * There are two ways to implement this:
-     * 1. Keep a stack of participants in the Interaction component.
-     * 2. Calculate the stack depth from the context.
-     *
-     * The latter is more testable.
-     *
-     * The API should be like this:
-     * const n: number = this.context?.stackDepth(this.source);
-     */
-    originOffset: function (): any {
-      return this.depthOnParticipant(this.origin) * OCCURRENCE_BAR_SIDE_WIDTH;
-    },
-    sourceOffset: function (): any {
-      return this.depthOnParticipant(this.source) * OCCURRENCE_BAR_SIDE_WIDTH;
-    },
-    targetOffset: function (): any {
-      return this.depthOnParticipant(this.target) * OCCURRENCE_BAR_SIDE_WIDTH;
     },
   },
 
