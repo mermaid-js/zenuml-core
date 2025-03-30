87bdd178d48adfbc0ea2a827c78b7594a78721b5
MrCoder <eagle.xiao@gmail.com>
2025-03-30 21:18:50 +1100

    fix(tailwind): update fragment header color and remove unused CSS variables
    - Update fragment header background color to use RGB format
    - Remove unused CSS variables for colors
    - Adjust Participant component to use new color scheme

--- src/components/DiagramFrame/SeqDiagram/LifeLineLayer/Participant.vue
+++ src/components/DiagramFrame/SeqDiagram/LifeLineLayer/Participant.vue
@@ -1,57 +1,46 @@
 <template>
   <div
-    v-if="isDefaultStarter"
-    class="participant bg-skin-participant shadow-participant border-transparent text-skin-participant rounded text-base leading-4 flex flex-col justify-center z-10 h-10 top-8"
+    class="participant bg-skin-participant shadow-participant border-skin-participant text-skin-participant rounded text-base leading-4 flex flex-col justify-center z-10 h-10 top-8"
     :class="{ selected: selected }"
     ref="participant"
     :style="{
+      backgroundColor: isDefaultStarter ? undefined : backgroundColor,
+      color: isDefaultStarter ? undefined : color,
       transform: `translateY(${translate}px)`,
     }"
     @click="onSelect"
   >
     <div
+      v-if="!!icon"
       v-html="icon"
-      class="text-skin-base bg-skin-frame px-1 absolute rounded left-1/2 transform -translate-x-1/2 h-8 [&>svg]:w-full [&>svg]:h-full"
+      class="px-1 absolute rounded left-1/2 transform -translate-x-1/2 h-8 [&>svg]:w-full [&>svg]:h-full"
+      :class="{
+        '-translate-y-full': !isDefaultStarter,
+        'bg-skin-frame': !isDefaultStarter,
+      }"
       :aria-description="`icon for ${entity.name}`"
     ></div>
-  </div>
-  <div v-else>
+
     <div
-      class="participant bg-skin-participant shadow-participant border-skin-participant text-skin-participant rounded text-base leading-4 flex flex-col justify-center z-10 h-10 top-8"
-      :class="{ selected: selected }"
-      ref="participant"
-      :style="{
-        backgroundColor: backgroundColor,
-        color: color,
-        transform: `translateY(${translate}px)`,
-      }"
-      @click="onSelect"
+      v-if="!isDefaultStarter"
+      class="h-5 group flex flex-col justify-center"
     >
-      <div
-        v-if="!!icon"
-        v-html="icon"
-        class="text-skin-base bg-skin-frame px-1 absolute rounded left-1/2 transform -translate-x-1/2 -translate-y-full h-8 [&>svg]:w-full [&>svg]:h-full"
-        :aria-description="`icon for ${entity.name}`"
-      ></div>
-      <!-- Put in a div to give it a fixed height, because stereotype is dynamic. -->
-      <div class="h-5 group flex flex-col justify-center">
-        <!-- TODO: create a better solution for participant comments -->
-        <!--      <span-->
-        <!--        v-if="!!comment"-->
-        <!--        class="absolute hidden rounded-lg transform -translate-y-8 bg-gray-400 px-2 py-1 text-center text-sm text-white group-hover:flex"-->
-        <!--      >-->
-        <!--        {{ comment }}-->
-        <!--      </span>-->
-        <label class="interface leading-4" v-if="stereotype"
-          >«{{ stereotype }}»</label
-        >
-        <ParticipantLabel
-          :labelText="labelText"
-          :labelPositions="labelPositions"
-          :assignee="entity.assignee"
-          :assigneePositions="assigneePositions"
-        />
-      </div>
+      <!-- TODO: create a better solution for participant comments -->
+      <!--      <span-->
+      <!--        v-if="!!comment"-->
+      <!--        class="absolute hidden rounded-lg transform -translate-y-8 bg-gray-400 px-2 py-1 text-center text-sm text-white group-hover:flex"-->
+      <!--      >-->
+      <!--        {{ comment }}-->
+      <!--      </span>-->
+      <label class="interface leading-4" v-if="stereotype"
+        >«{{ stereotype }}»</label
+      >
+      <ParticipantLabel
+        :labelText="labelText"
+        :labelPositions="labelPositions"
+        :assignee="entity.assignee"
+        :assigneePositions="assigneePositions"
+      />
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,47 +22,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        skin: {
-          base: "rgb(var(--color-bg-base-rgb))",
-          "text-base": "var(--color-text-base)",
-          "text-secondary": "var(--color-text-secondary)",
-          "border-base": "var(--color-border-base)",
-          "border-primary": "var(--color-border-primary)",
-          "frame-border": "var(--color-border-frame)",
-          "frame-bg":
-            "rgb(var(--color-bg-frame-rgb, var(--color-bg-canvas-rgb, var(--color-bg-base-rgb, var(--color-backup-white-rgb)))))",
-          "canvas-bg":
-            "rgb(var(--color-bg-canvas-rgb, var(--color-bg-base-rgb, var(--color-backup-white-rgb))))",
-          "title-bg":
-            "rgb(var(--color-bg-title-rgb, var(--color-bg-frame-rgb, var(--color-bg-canvas-rgb, var(--color-bg-base-rgb, var(--color-backup-white-rgb))))))",
-          "title-text": "var(--color-text-title)",
-          "participant-bg":
-            "rgb(var(--color-bg-participant-rgb, var(--color-bg-frame-rgb, var(--color-bg-canvas-rgb, var(--color-bg-base-rgb, var(--color-backup-white-rgb))))))",
-          "participant-text": "var(--color-text-participant)",
-          "participant-border": "var(--color-border-participant)",
-          "participant-shadow": "var(--color-shadow-participant)",
-          "message-text": "var(--color-text-message)",
-          "message-arrow": "var(--color-message-arrow)",
-          "message-hover-bg": "var(--color-bg-message-hover)",
-          "message-hover-text":
-            "rgb(var(--color-text-message-hover-rgb, var(--color-bg-base-rgb, var(--color-backup-white-rgb))))",
-          "comment-text": "var(--color-text-comment)",
-          "fragment-header-bg":
-            "rgb(var(--color-bg-fragment-header-rgb, var(--color-bg-participant-rgb, var(--color-bg-frame-rgb, var(--color-bg-canvas-rgb, var(--color-bg-base-rgb, var(--color-backup-white-rgb)))))))",
-          "fragment-text": "var(--color-text-fragment)",
-          "fragment-border": "var(--color-border-fragment)",
-          "occurrence-bg":
-            "rgb(var(--color-bg-occurrence-rgb, var(--color-bg-participant-rgb, var(--color-bg-frame-rgb, var(--color-bg-canvas-rgb, var(--color-bg-base-rgb, var(--color-backup-white-rgb)))))))",
-          "occurrence-border": "var(--color-border-occurrence)",
-          "occurrence-shadow": "var(--color-shadow-occurrence)",
-          "note-text": "var(--color-text-note)",
-          "note-bg": "var(--color-bg-note)",
-          "note-border": "var(--color-border-note)",
-          "link-text": "var(--color-text-link)",
-          "control-text": "var(--color-text-control)",
-        },
-      },
       textColor: {
         skin: {
           title:
@@ -107,7 +66,8 @@ module.exports = {
             "var(--color-border-participant, var(--color-border-frame, var(--color-border-base, #000)))",
           "message-hover":
             "var(--color-bg-message-hover, var(--color-text-base, #000))",
-          "fragment-header": "var(--color-bg-fragment-header, transparent)",
+          "fragment-header":
+            "rgb(var(--color-bg-fragment-header-rgb, 255, 255, 255))",
           occurrence:
             "rgb(var(--color-bg-occurrence-rgb, var(--color-bg-participant-rgb, var(--color-bg-frame-rgb, var(--color-bg-canvas-rgb, var(--color-bg-base-rgb, var(--color-backup-white-rgb)))))))",
           base: "rgb(var(--color-bg-base-rgb))",
