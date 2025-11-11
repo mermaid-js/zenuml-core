c12d6db3c6fcc04e93f87c7b77b156725592ea04
zicjin <zicjin@gmail.com>
2025-11-11 16:01:21 +0800

    async-message-mini-2.html

--- tests/async-message-mini.spec.ts
+++ tests/async-message-mini.spec.ts
@@ -2,9 +2,9 @@ import { test, expect } from "@playwright/test";
 import { initVerticalDebug, writeVerticalDebug } from "./utils/verticalDebug";
 
 test.describe("Rendering", () => {
-  test("Async message - mini", async ({ page }) => {
+  test("Async message mini - 1", async ({ page }) => {
     const didEnableDebug = await initVerticalDebug(page);
-    await page.goto("/cy/async-message-mini.html");
+    await page.goto("/cy/async-message-mini-1.html");
 
     // Wait for privacy icon to be loaded
     await expect(page.locator(".privacy>span>svg")).toBeVisible({
@@ -13,13 +13,35 @@ test.describe("Rendering", () => {
 
     // Take screenshot for visual comparison
     try {
-      await expect(page).toHaveScreenshot("async-message-mini.png", {
+      await expect(page).toHaveScreenshot("async-message-mini-1.png", {
         threshold: 0.01,
         fullPage: true,
       });
     } finally {
       if (didEnableDebug) {
-        await writeVerticalDebug(page, "async-message-mini-debug");
+        await writeVerticalDebug(page, "async-message-mini-1-debug");
+      }
+    }
+  });
+
+  test("Async message mini - 2", async ({ page }) => {
+    const didEnableDebug = await initVerticalDebug(page);
+    await page.goto("/cy/async-message-mini-2.html");
+
+    // Wait for privacy icon to be loaded
+    await expect(page.locator(".privacy>span>svg")).toBeVisible({
+      timeout: 5000,
+    });
+
+    // Take screenshot for visual comparison
+    try {
+      await expect(page).toHaveScreenshot("async-message-mini-2.png", {
+        threshold: 0.01,
+        fullPage: true,
+      });
+    } finally {
+      if (didEnableDebug) {
+        await writeVerticalDebug(page, "async-message-mini-2-debug");
       }
     }
   });
