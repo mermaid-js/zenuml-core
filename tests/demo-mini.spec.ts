6ad54603e6c379c4a3678ebef89e952280d3871f
Cheney Jin <zicjin@gmail.com>
2025-11-16 22:03:01 +0800

    remove demo-mini-2

--- tests/demo-mini.spec.ts
+++ tests/demo-mini.spec.ts
@@ -21,24 +21,4 @@ test.describe("Rendering", () => {
       }
     }
   });
-
-  test("demo-mini-2", async ({ page }) => {
-    const didEnableDebug = await initVerticalDebug(page);
-    await page.goto("http://127.0.0.1:8080/cy/demo-mini-2.html");
-
-    await expect(page.locator(".privacy>span>svg")).toBeVisible({
-      timeout: 5000,
-    });
-
-    try {
-      await expect(page).toHaveScreenshot("demo-mini-2.png", {
-        threshold: 0.02,
-        fullPage: true,
-      });
-    } finally {
-      if (didEnableDebug) {
-        await writeVerticalDebug(page, "demo-mini-2-debug");
-      }
-    }
-  });
 });
