19dde40cf38b0ea76d1f0db11ca943dafe3e2ebc
Peng Xiao <eagle.xiao@gmail.com>
2025-11-19 11:52:28 +1100

    feat: refactor git hash injection and improve debug component (#311)
    * feat: add git hash and branch information to Vite config
    
    * feat: refactor git hash injection and improve debug component

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APP_GIT_HASH: string;
+    readonly VITE_APP_GIT_BRANCH: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
