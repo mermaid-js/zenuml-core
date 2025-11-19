91718232b8e17637528857465eb8526942a039bd
MrCoder <eagle.xiao@gmail.com>
2025-11-19 11:50:21 +1100

    feat: refactor git hash injection and improve debug component

--- src/components/DiagramFrame/Debug/index.tsx
+++ src/components/DiagramFrame/Debug/index.tsx
@@ -1,21 +1,14 @@
 import Icon from "@/components/Icon/Icons";
 
 export const Debug = () => {
-  const commitHash = import.meta.env.VITE_APP_GIT_HASH;
-  const gitBranch = import.meta.env.VITE_APP_GIT_BRANCH;
-
-  if (!localStorage.zenumlDebug) {
-    return null;
-  }
+  if (!localStorage.zenumlDebug) return null;
 
   return (
-    <div className="flex flex-nowrap m-2 text-sm">
-      <div className="ml-4 text-xs inline-flex items-center font-bold leading-sm px-3 py-1 bg-green-200 text-green-700 rounded-sm">
-        <Icon name="debug" className="h-4 w-4" />
-        <span className="inline-block px-2">
-          {gitBranch}:{commitHash}
-        </span>
-      </div>
+    <div className="text-xs inline-flex items-center font-mono font-medium leading-sm mx-1 px-3 py-1 bg-slate-100 text-slate-500 rounded-sm">
+      <Icon name="debug" className="h-3 w-3" />
+      <span className="inline-block px-2">
+        {import.meta.env.VITE_APP_GIT_BRANCH}:{import.meta.env.VITE_APP_GIT_HASH}
+      </span>
     </div>
   );
 };
