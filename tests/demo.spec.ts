7610ec59562c1a5cb2f1423de71d89d541827b6c
Cheney Jin <zicjin@gmail.com>
2025-11-20 22:25:32 +0800

    demo-mini-2

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from "@playwright/test";
 import { initVerticalDebug, writeVerticalDebug } from "./utils/verticalDebug";
 
 const demos = [
-  {
-    name: "Demo1 DSL",
-    url: "http://127.0.0.1:8080/cy/demo1.html",
-    snapshot: "demo1.png",
-    debugSlug: "demo1-debug",
-  },
+  // {
+  //   name: "Demo1 DSL",
+  //   url: "http://127.0.0.1:8080/cy/demo1.html",
+  //   snapshot: "demo1.png",
+  //   debugSlug: "demo1-debug",
+  // },
   {
     name: "Demo3 DSL",
     url: "http://127.0.0.1:8080/cy/demo3.html",
