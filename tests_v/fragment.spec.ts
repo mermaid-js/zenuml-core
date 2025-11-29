b48f96e6aa96cd4407f2cf77b4101fe98a0385f4
Cheney Jin <zicjin@gmail.com>
2025-11-29 13:08:55 +0800

    temp-13

--- tests_v/fragment.spec.ts
+++ tests_v/fragment.spec.ts
@@ -41,4 +41,64 @@ test.describe("Fragments", () => {
       }
     }
   });
+
+  test("loop fragment", async ({ page }) => {
+    const didEnableDebug = await initVerticalDebug(page);
+    await page.goto("http://127.0.0.1:8080/cy/fragment-loop.html");
+
+    await expect(page.locator(".privacy>span>svg")).toBeVisible({
+      timeout: 5000,
+    });
+
+    try {
+      await expect(page).toHaveScreenshot("fragment-loop.png", {
+        threshold: 0.02,
+        fullPage: true,
+      });
+    } finally {
+      if (didEnableDebug) {
+        await writeVerticalDebug(page, "fragment-loop");
+      }
+    }
+  });
+
+  test("par fragment", async ({ page }) => {
+    const didEnableDebug = await initVerticalDebug(page);
+    await page.goto("http://127.0.0.1:8080/cy/fragment-par.html");
+
+    await expect(page.locator(".privacy>span>svg")).toBeVisible({
+      timeout: 5000,
+    });
+
+    try {
+      await expect(page).toHaveScreenshot("fragment-par.png", {
+        threshold: 0.02,
+        fullPage: true,
+      });
+    } finally {
+      if (didEnableDebug) {
+        await writeVerticalDebug(page, "fragment-par");
+      }
+    }
+  });
+
+  test("nested alt with opt and loop", async ({ page }) => {
+    const didEnableDebug = await initVerticalDebug(page);
+    await page.goto("http://127.0.0.1:8080/cy/fragment-alt-nested.html");
+
+    await expect(page.locator(".privacy>span>svg")).toBeVisible({
+      timeout: 5000,
+    });
+
+    try {
+      await expect(page).toHaveScreenshot("fragment-alt-nested.png", {
+        threshold: 0.02,
+        fullPage: true,
+      });
+    } finally {
+      if (didEnableDebug) {
+        await writeVerticalDebug(page, "fragment-alt-nested");
+      }
+    }
+  });
 });
