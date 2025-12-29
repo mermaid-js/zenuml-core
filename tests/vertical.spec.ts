fafc0c86c00d24196df21fd5e3f7a52a255c0d75
Cheney Jin <zicjin@gmail.com>
2025-12-29 11:05:23 +0800

    vertical.spec.ts

--- tests/vertical.spec.ts
+++ tests/vertical.spec.ts
@@ -26,4 +26,56 @@ test.describe("Rendering", () => {
       fullPage: true,
     });
   });
+
+  test("vertical-3", async ({ page }) => {
+    await page.goto("/cy/vertical-3.html");
+
+    await expect(page.locator(".privacy>span>svg")).toBeVisible({
+      timeout: 5000,
+    });
+
+    await expect(page).toHaveScreenshot("vertical-3.png", {
+      threshold: 0.02,
+      fullPage: true,
+    });
+  });
+
+  test("vertical-4", async ({ page }) => {
+    await page.goto("/cy/vertical-4.html");
+
+    await expect(page.locator(".privacy>span>svg")).toBeVisible({
+      timeout: 5000,
+    });
+
+    await expect(page).toHaveScreenshot("vertical-4.png", {
+      threshold: 0.02,
+      fullPage: true,
+    });
+  });
+
+  test("vertical-5", async ({ page }) => {
+    await page.goto("/cy/vertical-5.html");
+
+    await expect(page.locator(".privacy>span>svg")).toBeVisible({
+      timeout: 5000,
+    });
+
+    await expect(page).toHaveScreenshot("vertical-5.png", {
+      threshold: 0.02,
+      fullPage: true,
+    });
+  });
+
+  test("vertical-6", async ({ page }) => {
+    await page.goto("/cy/vertical-6.html");
+
+    await expect(page.locator(".privacy>span>svg")).toBeVisible({
+      timeout: 5000,
+    });
+
+    await expect(page).toHaveScreenshot("vertical-6.png", {
+      threshold: 0.02,
+      fullPage: true,
+    });
+  });
 });
